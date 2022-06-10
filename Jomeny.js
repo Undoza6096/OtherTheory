036c065274a8ee4ef822c74ab2e93ceb4b6a9762
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-09 22:52:48 -0300

    Update and rename NerIdlerOld.js to Jomeny.js

---
+++ Jomeny.js
@@ -0,0 +1,147 @@
+import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
+import { Localization } from "./api/Localization";
+import { BigNumber } from "./api/BigNumber";
+import { theory } from "./api/Theory";
+import { Utils } from "./api/Utils";
+
+var id = "my_custom_theory_id";
+var name = "Jomeny";
+var description = "A basic theory.";
+var authors = "Annotations6";
+var version = 1;
+
+var currency;
+var c1, c2, c3, aplha1;
+var c1Exp, c2Exp;
+
+var achievement1, achievement2;
+var chapter1, chapter2;
+
+var init = () => {
+    currency = theory.createCurrency();
+    currency2 = theory.createCurrency();
+    currency_heart3 = theory.createCurrency("🥰", "🥰");
+
+    ///////////////////
+    // Regular Upgrades
+
+    // c1
+    {
+        let getDesc = (level) => "c_1=" + getC1(level).toString(0);
+        c1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(20, Math.log2(100))));
+        c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
+        c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
+        c1.maxLevel = 85;
+    }
+
+    // c2
+    {
+        let getDesc = (level) => "c_2=\\aplha_1^{2}+3^{" + level + "}";
+        let getInfo = (level) => "c_2=" + getC2(level).toString(0);
+        c2 = theory.createUpgrade(1, currency, new ExponentialCost(5, Math.log2(10)));
+        c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
+        c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
+    }
+
+    // c3
+    {
+        let getDesc = (level) => "c_3=\\alpha_1^{3}+3^{" + level + "}";
+        let getInfo = (level) => "c_3=" + getC3(level).toString(0);
+        c3 = theory.createUpgrade(2, currency, new ExponentialCost(100000000, Math.log2(1e3)));
+        c3.getDescription = (_) => Utils.getMath(getDesc(c3.level));
+        c3.getInfo = (amount) => Utils.getMathTo(getInfo(c3.level), getInfo(c3.level + amount));
+    }
+
+    // alpha1
+    {
+        let getDesc = (level) => "\\alpha_1=" + level
+        let getInfo = (level) => "c_3=" + getAPLHA1(level).toString(0);
+        alpha1 = theory.createUpgrade(2, currency_heart3, new ExponentialCost(1.25, Math.log2(1.12)));
+        alpha1.getDescription = (_) => Utils.getMath(getDesc(alpha1.level));
+        alpha1.getInfo = (amount) => Utils.getMathTo(getInfo(alpha1.level), getInfo(alpha1.level + amount));
+    }
+
+    /////////////////////
+    // Permanent Upgrades
+    theory.createPublicationUpgrade(0, currency, 1e10);
+    theory.createBuyAllUpgrade(1, currency, 1e13);
+    theory.createAutoBuyerUpgrade(2, currency, 1e30);
+
+    ///////////////////////
+    //// Milestone Upgrades
+    theory.setMilestoneCost(new LinearCost(25, 25));
+
+    {
+        c1Exp = theory.createMilestoneUpgrade(0, 3);
+        c1Exp.description = Localization.getUpgradeIncCustomExpDesc("c_1", "0.05");
+        c1Exp.info = Localization.getUpgradeIncCustomExpInfo("c_1", "0.05");
+        c1Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
+    }
+
+    {
+        c2Exp = theory.createMilestoneUpgrade(1, 3);
+        c2Exp.description = Localization.getUpgradeIncCustomExpDesc("c_2", "0.05");
+        c2Exp.info = Localization.getUpgradeIncCustomExpInfo("c_2", "0.05");
+        c2Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
+    }
+    
+    /////////////////
+    //// Achievements
+    achievement1 = theory.createAchievement(0, "\u2800", "start this theory.", () => c1.level > 0);
+    achievement2 = theory.createAchievement(1, "Upgrade 1", "while", () => c1.level > 1);
+    achievement3 = theory.createAchievement(2, "Trandozilus", "yes me", () => c1.level > 8);
+    achievement4 = theory.createAchievement(3, "First Decillion", "funny", () => currency.value > 1e33);
+    achievement5 = theory.createAchievement(4, "Somenthing?", "Reach 2 🥰 (somehow?)", () => currency_heart3.value > 2);
+    achievement6 = theory.createAchievement(5, "Over Slimes what", "reach waitttttttt", () => currency_heart3.value > 2.1e25);
+
+    ///////////////////
+    //// Story chapters
+    chapter1 = theory.createStoryChapter(0, "My First Chapter", "This is line 1,\nand this is line 2.\n\nNice.", () => c1.level > 0);
+    chapter2 = theory.createStoryChapter(1, "My Second Chapter", "This is line 1 again,\nand this is line 2... again.\n\nNice again.", () => c2.level > 0);
+
+    updateAvailability();
+}
+
+var updateAvailability = () => {
+    c2Exp.isAvailable = c1Exp.level > 0;
+}
+
+var tick = (elapsedTime, multiplier) => {
+    let dt = BigNumber.from(elapsedTime * multiplier);
+    let bonus = theory.publicationMultiplier;
+    currency_heart3 = currency2.pow(0.01);
+    currency2.value = currency.value.pow(0.1);
+    currency.value += dt * bonus * getC1(c1.level) * getC2(c2.level);
+                                   
+}
+
+var getPrimaryEquation = () => {
+    let result = "\\dot{\\rho} = c_1";
+
+    if (c1Exp.level == 1) result += "^{1.05}";
+    if (c1Exp.level == 2) result += "^{1.1}";
+    if (c1Exp.level == 3) result += "^{1.15}";
+
+    result += "c_2";
+
+    if (c2Exp.level == 1) result += "^{1.05}";
+    if (c2Exp.level == 2) result += "^{1.1}";
+    if (c2Exp.level == 3) result += "^{1.15}";
+
+    return result;
+}
+
+var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho";
+var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
+var getTau = () => currency.value;
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
+
+var getC1 = (level) => Utils.getStepwisePowerSum(level, 5, 10, 0);
+var getC2 = (level) => BigNumber.THREE.pow(level) + getAPLHA1(aplha1.level).pow(2);
+var getC3 = (level) => BigNumber.THREE.pow(level) + getAPLHA1(aplha1.level).pow(3);
+var getAPLHA1 = (level) => BigNumber.from(5 * level);
+var getC1Exponent = (level) => BigNumber.from(1 + 0.05 * level);
+var getC2Exponent = (level) => BigNumber.from(1 + 0.05 * level);
+
+init();
